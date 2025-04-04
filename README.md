6b74ca53cc41f059254dd6dd5dc15648c93aab8d
Nick Parrish <NickParrish@creativeliquidcoatings.com>
2025-04-03 22:46:55 -0400

    Inital Commit - Node setup to render index.html

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mixed Messages
+
+## Description
+This is a learning project. The site will generate a message each time the user executes the script.
---
+++ app.js
@@ -0,0 +1,20 @@
+// Requiring module
+const express = require('express');
+// creating express object
+const app = express();
+// Requiring path module
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Add the router
+app.use('/', router);
+
+// Port Number
+const PORT = process.env.PORT || 3000;
+
+// Server setup
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
